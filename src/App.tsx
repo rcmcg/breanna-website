c253b423c72428eb220823a891bb6a048cd6fff7
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-30 16:28:19 -0800

    add preview cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,14 @@
 import './App.css'
-import {type JSX, useState} from "react";
+import type {JSX} from "react";
 
-const personalImages = [
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-  "https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg",
-]
 const personalElements: JSX.Element[] = []
-for (let index= 0; index < personalImages.length; index++) {
+for (let index= 0; index < 19; index++) {
   personalElements.push(
-    <div key={index + "_personal"} className={"gallery-item"}>
-      <img
-        src={personalImages[index]}
-        alt={"Image from the Public Domain Archive"}
-      />
-    </div>
-  )
-}
-
-const professionalImages: string[] = [
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-  "https://cdn.pixabay.com/photo/2022/08/08/19/36/landscape-7373484_1280.jpg",
-]
-const professionalElements: JSX.Element[] = []
-for (let index= 0; index < professionalImages.length; index++) {
-  professionalElements.push(
-    <div key={index + "_prof"} className={"gallery-item"}>
-      <img
-        src={professionalImages[index]}
-        alt={"null"}
-      />
+    <div
+      key={index + "_personal"}
+      className={"gallery-item" + " img-" + index}
+      // style={{backgroundImage: `url(${personalImages[index]})`}}
+    >
     </div>
   )
 }
@@ -58,26 +24,14 @@ function Header() {
   )
 }
 
-const allImages = [personalElements, professionalElements]
-
 function App() {
-  const [imageIndex, setImageIndex] = useState(0);
-  const [images, setImages] = useState(personalElements);
-
-  function handleClick() {
-    setImageIndex((imageIndex: number) => {
-      return (imageIndex + 1) % 2
-    })
-    setImages(allImages[imageIndex])
-  }
 
   return (
     <>
       <Header />
       <div className={"main"}>
-        <button onClick={handleClick}>Change images</button>
         <div className={"gallery-container"}>
-          {images}
+          {personalElements}
         </div>
       </div>
     </>
