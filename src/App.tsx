85072dcc07ae0b2a71482a6df5e0cbdd18af2ee8
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-31 11:04:21 -0800

    WIP modal dialog, not really working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
 import './App.css'
-import type {JSX} from "react";
-
-const personalElements: JSX.Element[] = []
-for (let index= 0; index < 19; index++) {
-  personalElements.push(
-    <div
-      key={index + "_personal"}
-      className={"gallery-item" + " img-" + index}
-      // style={{backgroundImage: `url(${personalImages[index]})`}}
-    >
-    </div>
-  )
-}
+import {type JSX, useLayoutEffect, useRef, useState} from "react";
 
 function Header() {
 
@@ -35,15 +23,65 @@ function Header() {
   )
 }
 
+function Dialog({isOpen, imgUrl}: {isOpen: boolean, imgUrl: string}) {
+  return (
+    <div>
+      <dialog open={isOpen}>
+        <img src={imgUrl}/>
+      </dialog>
+    </div>
+  )
+}
+
+function Gallery() {
+  const personalElements: JSX.Element[] = []
+  for (let index= 0; index < 19; index++) {
+    personalElements.push(
+      <GalleryItem index={index} />
+    )
+  }
+
+  return (
+    <div className={"gallery-container"}>
+      {personalElements}
+    </div>
+  )
+}
+
+function GalleryItem({index}: {index: number}) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useLayoutEffect(() => {
+    if (dialogRef.current?.open && !isDialogOpen) {
+      dialogRef.current.close()
+    } else if (dialogRef.current?.open && isDialogOpen) {
+      dialogRef.current.showModal()
+    }
+  }, [isDialogOpen]);
+
+  return (
+    <>
+      <Dialog isOpen={isDialogOpen} imgUrl={"/personal/preview/knight-preview.png"}/>
+      <div
+        key={index + "_personal"}
+        className={"gallery-item" + " img-" + index}
+        onClick={() => {setIsDialogOpen(!isDialogOpen)}}
+        // style={{backgroundImage: `url(${personalImages[index]})`}}
+      >
+      </div>
+    </>
+  )
+}
+
 function App() {
 
   return (
     <>
       <Header />
       <div className={"main"}>
-        <div className={"gallery-container"}>
-          {personalElements}
-        </div>
+        <Gallery />
       </div>
     </>
   )
